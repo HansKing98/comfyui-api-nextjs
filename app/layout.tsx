2b3bd4d131b5b4b369423554db92570b186042f2
hans <im.hansking@gmail.com>
2024-03-30 14:26:54 +0800

    no message

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <p className="h-24 left-0 top-0 flex w-full justify-center  from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:from-inherit lg:w-auto  lg:rounded-xl  lg:p-4 "></p>
         <p className="fixed top-8 left-32 dark:border-neutral-800 dark:from-inherit lg:w-auto  lg:rounded-xl ">
           <Link className="text-2xl" href="/">
-            COMFYUI-NEXTJS_API
+            COMFYUI-API-NEXTJS
           </Link>
         </p>
         <div className="fixed top-0 flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            START GENERATE{" "}
+            GENERATE{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
